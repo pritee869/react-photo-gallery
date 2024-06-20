de46db39d9a00bde0e6f81c2b4715ffa48e010d6
pritee869 <152505445+pritee869@users.noreply.github.com>
2024-06-20 10:33:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GalleryPhotos from './components/GalleryPhotos';
+
+function App() {
+  return (
+    <div className="App">
+    <GalleryPhotos/>
+    </div>
+  );
+}
+
+export default App;
